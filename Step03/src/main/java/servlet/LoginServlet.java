9d82ed40512f5332e0f316423fd983856e49b7de
NamSangKyu <nam2626@naver.com>
2026-05-08 11:54:39 +0900

    Add LoginServlet and JSP pages for user authentication

---
+++ Step03/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,49 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+		HttpSession session = request.getSession();//세션 정보 가져옴
+		if(id.equals("admin") && passwd.equals("123456")) {
+			session.setAttribute("msg", id + "님 환영합니다.");
+			response.sendRedirect("./login_success.jsp");
+		}else {
+			response.sendRedirect("./login_fail.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
