e97910f4c85ce7e78617af51264403171df8c305
NamSangKyu <nam2626@naver.com>
2026-05-11 10:11:54 +0900

    Refactor HandlerMapping to use command strings for controller creation

--- student-mvc/src/main/java/controller/HandlerMapping.java
+++ student-mvc/src/main/java/controller/HandlerMapping.java
@@ -15,33 +15,14 @@ public class HandlerMapping {
 		return instance;
 	}
 	
-	/**
-	 * 메뉴 번호에 해당하는 작업을 하는 컨트롤러를 생성해서 리턴하는 메서드
-	 * @param no 사용자가 입력한 메뉴 번호
-	 * @return 실행할 컨트롤러 객체 (해당 번호가 없으면 null 반환)
-	 */
-	public Controller createController(int no) {
+	public Controller createController(String command) {
 		Controller controller = null;
-		switch(no) {
-		case 1:
-			// 학생 정보 등록을 담당하는 컨트롤러 생성
-			controller = new StudentInsertController();
-			break;
-		case 2:
-			// 학생 정보 삭제를 담당하는 컨트롤러 생성
-			controller = new StudentDeleteController();
-			break;
-		case 3:
-			controller = new StudentUpdateController();
-			break;
-		case 4:
-			controller = new StudentSearchForNameController();
-			break;
-		case 5:
-			// 전체 학생 정보 출력을 담당하는 컨트롤러 생성
-			controller = new StudentAllPrintController();
-			break;
+		switch(command) {
 		}
 		return controller;
 	}
 }
+
+
+
+
