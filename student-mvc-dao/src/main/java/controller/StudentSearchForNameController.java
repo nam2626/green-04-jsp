1fdb9ddbe6fc85c3fb0acc2ae88e502f9777b03e
NamSangKyu <nam2626@naver.com>
2026-05-12 14:33:27 +0900

    Update StudentSearchForNameController to return a list of students based on name search

--- student-mvc-dao/src/main/java/controller/StudentSearchForNameController.java
+++ student-mvc-dao/src/main/java/controller/StudentSearchForNameController.java
@@ -1,7 +1,7 @@
 package controller;
 
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,18 +9,15 @@ import service.StudentService;
 import view.ModelAndView;
 import vo.StudentVO;
 
-/**
- * 이름을 입력받아 해당 학생 정보를 검색하여 출력하는 기능을 담당하는 컨트롤러
- */
 public class StudentSearchForNameController implements Controller {
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String name = request.getParameter("name");
 		
-		StudentVO vo = StudentService.getInstance().searchStudentVOForName(name);
-		request.setAttribute("vo", vo);
+		ArrayList<StudentVO> list = StudentService.getInstance().searchStudentVOForName(name);
+		request.setAttribute("list", list);
 		
-		return new ModelAndView("search_result", false);
+		return new ModelAndView("main", false);
 	}
 }
 
