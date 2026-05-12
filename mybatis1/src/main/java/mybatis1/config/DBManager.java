166532ab7027dc08ea5bc375191c8a5e907fe7f7
NamSangKyu <nam2626@naver.com>
2026-05-12 15:10:15 +0900

    Add DBManager class for MyBatis SQL session factory initialization

--- mybatis1/src/main/java/mybatis1/config/DBManager.java
+++ mybatis1/src/main/java/mybatis1/config/DBManager.java
@@ -1,9 +1,26 @@
 package mybatis1.config;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class DBManager {
 	private static DBManager instance = new DBManager();
 
+	private SqlSessionFactory sqlSessionFactory;
+	
 	private DBManager() {
+		String resource = "org/mybatis/example/mybatis-config.xml";
+		InputStream inputStream;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static DBManager getInstance() {
