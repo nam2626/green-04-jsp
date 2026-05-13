3a88e0780c5c033d8964370589738b9b1a10d492
NamSangKyu <nam2626@naver.com>
2026-05-13 17:48:25 +0900

    Refactor CheckIdController to set response data in JSON format and remove debug print statement

--- member-project/src/main/java/controller/CheckIdController.java
+++ member-project/src/main/java/controller/CheckIdController.java
@@ -1,18 +1,41 @@
 package controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+
+import org.json.JSONObject;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import service.MemberService;
 import view.ModelAndView;
 
 public class CheckIdController implements Controller {
 
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		System.out.println(request.getParameter("id"));
+//		System.out.println(request.getParameter("id"));
+		//사용자에게 보낼 데이터를 셋팅
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("result", 1);
+		map.put("msg", "결과 메세지");
+		map.put("list", MemberService.getInstance().selectAllMember());
 		
+		JSONObject json = new JSONObject(map);
+		System.out.println(json.toString());
 		return null;
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
