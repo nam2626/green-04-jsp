7242252ba2fd8633b1fdd7b1a618616a2c42b421
NamSangKyu <nam2626@naver.com>
2026-05-13 14:38:17 +0900

    Implement Controller interface and HandlerMapping for request handling

---
+++ member-project/src/main/java/controller/HandlerMapping.java
@@ -0,0 +1,35 @@
+package controller;
+
+/**
+ * 사용자의 메뉴 선택(no)에 따라 적절한 컨트롤러 객체를 생성하여 반환하는 클래스 싱글톤 패턴으로 구현되어 프로그램 전체에서 하나의 매핑
+ * 정보만 관리함
+ */
+public class HandlerMapping {
+	private static HandlerMapping instance = new HandlerMapping();
+
+	private HandlerMapping() {
+	}
+
+	public static HandlerMapping getInstance() {
+		if (instance == null)
+			instance = new HandlerMapping();
+		return instance;
+	}
+
+	public Controller createController(String command) {
+		Controller controller = null;
+		switch (command) {
+		
+		}
+		return controller;
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ member-project/src/main/java/service/MemberService.java
@@ -0,0 +1,19 @@
+package service;
+
+import config.DBManager;
+import mapper.MemberMapper;
+
+public class MemberService {
+	private static MemberService instance = new MemberService();
+	private MemberMapper mapper;
+	
+	private MemberService() {	
+		mapper = DBManager.getInstance().getSession().getMapper(MemberMapper.class);
+	}
+	
+	public static MemberService getInstance() {
+		if(instance == null)
+			instance = new MemberService();
+		return instance;
+	}
+}
