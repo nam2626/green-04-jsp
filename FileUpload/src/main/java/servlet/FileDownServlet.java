3f9b9860e4b87d591a454f1d7315b2fe886e449a
NamSangKyu <nam2626@naver.com>
2026-05-15 15:03:38 +0900

    feat: implement file download functionality in FileDownServlet

--- FileUpload/src/main/java/servlet/FileDownServlet.java
+++ FileUpload/src/main/java/servlet/FileDownServlet.java
@@ -6,7 +6,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -40,7 +42,21 @@ public class FileDownServlet extends HttpServlet {
 		response.setContentLengthLong(file.length());
 		
 		//스트림을 이용해서 파일을 읽고, 클라이언트에게 전송
-		
+		try(FileInputStream fis = new FileInputStream(file);
+			BufferedOutputStream bos = 
+					new BufferedOutputStream(response.getOutputStream())){
+			byte[] buffer = new byte[1024*1024];
+			
+			while(true) {
+				int size = fis.read(buffer);
+				if(size == -1) break;
+				bos.write(buffer, 0, size);
+				bos.flush();
+				
+			}
+			
+			
+		}
 		
 	}
 
