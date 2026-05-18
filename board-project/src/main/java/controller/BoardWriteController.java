a02836827a562eb941373471102d24b3dd5cf953
NamSangKyu <nam2626@naver.com>
2026-05-18 17:04:24 +0900

    feat: update BoardWriteController to handle session and post data

--- board-project/src/main/java/controller/BoardWriteController.java
+++ board-project/src/main/java/controller/BoardWriteController.java
@@ -2,8 +2,11 @@ package controller;
 
 import java.io.IOException;
 
+import dto.BoardDTO;
+import dto.MemberDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import view.ModelAndView;
 
 public class BoardWriteController implements Controller {
@@ -11,9 +14,19 @@ public class BoardWriteController implements Controller {
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		//로그인한 계정의 회원번호 읽어옴
-		
+		HttpSession session = request.getSession();
+		MemberDTO member = (MemberDTO) session.getAttribute("user");
+		int mno = member.getNo();
 		//title, content
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
 		
+		BoardDTO board = new BoardDTO();
+		board.setMno(mno);
+		board.setTitle(title);
+		board.setContent(content);
+		
+		System.out.println(board);
 		//파일 쓰기 처리
 		
 		//board, board_file 내용을 등록
