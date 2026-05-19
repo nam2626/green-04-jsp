9d107099a7406e7204b4bccd052b3fddf25e7d00
NamSangKyu <nam2626@naver.com>
2026-05-19 17:14:31 +0900

    feat: add methods for inserting, deleting, and counting likes in BoardService

--- board-project/src/main/java/service/BoardService.java
+++ board-project/src/main/java/service/BoardService.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import config.DBManager;
@@ -47,6 +48,24 @@ public class BoardService {
 
 	public int updateBoardCount(int bno) {
 		return mapper.updateBoardCount(bno);
+	}
+
+	public int insertBoardLike(int no, int bno) {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("bno", bno);
+		map.put("mno", no);
+		return mapper.insertBoardLike(map);
+	}
+
+	public int deleteBoardLike(int no, int bno) {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("bno", bno);
+		map.put("mno", no);
+		return mapper.deleteBoardLike(map);
+	}
+
+	public int boardLikeCount(int bno) {
+		return mapper.boardLikeCount(bno);
 	}
 }
 
